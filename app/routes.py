c8b8eb1597e979a6db6cc1bc89c936ede65ea032
JorgeToAn <jorgetosta.f@gmail.com>
2022-09-16 21:55:24 -0700

    Connected backend and frontend

--- app/routes.py
+++ app/routes.py
@@ -8,4 +8,4 @@ BACKEND_URL = "http://127.0.0.1:5000/data_types"
 @app.get("/")
 def get_index():
     scan_data = requests.get(BACKEND_URL)
-    return render_template("main.html", data_types=scan_data)
+    return render_template("index.html", data_types=scan_data.json())
